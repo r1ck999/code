c715d8a5068c9ff99e0a00047604cc89acca3dc5
r999 <124431219+r1ck999@users.noreply.github.com>
2024-05-15 07:37:50 +0530

    Update

---
+++ dfs.py
@@ -0,0 +1,22 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example graph represented as adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Start DFS traversal from node 'A'
+dfs(graph, 'A')
